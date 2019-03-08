43bc984a606e0531a9f5d5b537f7b8029ab3a0f6
inglec <inglec@tcd.ie>
2019-03-08 16:37:08 +0000

    Routes: Rewrite sections

--- src/routes/login.route.js
+++ src/routes/login.route.js
@@ -1,7 +1,9 @@
-const bcrypt = require('bcryptjs');
-const express = require('express');
-const { BAD_REQUEST } = require('http-status-codes');
-const jwt = require('jsonwebtoken');
+const { compare } = require('bcryptjs');
+const { Router } = require('express');
+const { BAD_REQUEST, INTERNAL_SERVER_ERROR } = require('http-status-codes');
+const { sign } = require('jsonwebtoken');
+const { pick } = require('lodash');
+const { default: createLogger } = require('logging');
 
 const User = require('../models/user.model');
 
@@ -10,49 +12,49 @@ const {
   TOKEN_EXPIRY_TIME: tokenExpiryTime = 3600,
 } = process.env;
 
-const router = express.Router();
+const logger = createLogger('tt:auth');
+const router = Router();
 
 router.post('/', (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
-    res
-      .status(BAD_REQUEST)
-      .json({ message: 'request missing email/password field(s)' });
+    res.status(BAD_REQUEST).json({ message: 'missing email/password field(s)' });
   } else {
     User
       .findOne({ email })
       .then((user) => {
         if (!user) {
-          throw new Error('User not found.');
+          res.status(BAD_REQUEST).json({ message: 'user not found' });
         } else {
           // Validate password
-          bcrypt.compare(password, user.password)
+          compare(password, user.password)
             .then((isCorrect) => {
               if (!isCorrect) {
                 res.status(BAD_REQUEST).json({ message: 'invalid login details' });
               } else {
-                // Create JWT, including how long until it expires.
-                jwt.sign(
-                  { id: user.id },
-                  jwtSecret,
-                  { expiresIn: tokenExpiryTime },
-                  (error, token) => {
-                    if (error) {
-                      throw error;
-                    }
-                    res.json({
-                      token,
-                      user: {
-                        email: user.email,
-                        userType: user.userType,
-                      },
-                    });
-                  },
-                );
+                const payload = { id: user.id };
+                const options = { expiresIn: tokenExpiryTime };
+                const callback = (error, token) => {
+                  if (error) {
+                    throw error;
+                  }
+
+                  res.json({
+                    token,
+                    user: pick(user, ['email', 'userType']),
+                  });
+                };
+
+                // Create JWT, including how long until it expires
+                sign(payload, jwtSecret, options, callback);
               }
             });
         }
-      }).catch(() => res.status(BAD_REQUEST).json({ message: 'user not found' }));
+      }).catch((error) => {
+        res.status(INTERNAL_SERVER_ERROR).end();
+        logger.error(error);
+      });
   }
 });
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,38 +1,43 @@
-const bcrypt = require('bcryptjs');
-const express = require('express');
-const { BAD_REQUEST } = require('http-status-codes');
+const { genSalt, hash } = require('bcryptjs');
+const { Router } = require('express');
+const { BAD_REQUEST, INTERNAL_SERVER_ERROR, UNAUTHORIZED } = require('http-status-codes');
+const { pick } = require('lodash');
+const { default: createLogger } = require('logging');
 
 const auth = require('../middleware/auth.js');
 const User = require('../models/user.model');
 
-const ZERO = 0;
-const router = express.Router();
+const logger = createLogger('tt:route');
+const router = Router();
 
 router.get('/', auth, (req, res) => {
   const { email } = req.body;
+  const { id: userId } = req.user;
 
-  User.findById(req.user.id)
+  User
+    .findById(userId)
     .select('-password')
     .then((user) => {
       if (!email) {
-        res.status(BAD_REQUEST).json({ message: 'Missing required parameter: email' });
+        res.status(BAD_REQUEST).json({ message: 'missing required parameter: email' });
       } else if (email === user.email || user.userType === 'nurse') {
-        User.find()
+        User
+          .find()
           .where({ email })
           .select('-password')
           .then((users) => {
-            if (users.length) {
-              res.json(users[ZERO]);
+            if (users.length > 0) {
+              res.json(users[0]);
             } else {
               res
                 .status(BAD_REQUEST)
-                .json({ message: 'No users found' });
+                .json({ message: 'no users found' });
             }
           });
       } else {
         res
-          .status(401)
-          .json({ message: 'Permission denied' });
+          .status(UNAUTHORIZED)
+          .json({ message: 'permission denied' });
       }
     });
 });
@@ -43,51 +48,48 @@ router.post('/', auth, (req, res) => {
   if (!email || !password || !userType) {
     res
       .status(BAD_REQUEST)
-      .json({ message: 'Request body is missing required field.' });
+      .json({ message: 'request body is missing required field' });
   } else {
-    User.findOne({ email }).then((user) => {
-      if (user) {
-        res
-          .status(BAD_REQUEST)
-          .json({ message: 'User with this email already exists.' });
-      } else {
-        const newUser = new User({
-          email,
-          password,
-          userType,
-        });
-
-        // hashing password
-        bcrypt.genSalt(10, (err, salt) => {
-          if (err) {
-            throw err;
-          }
-          bcrypt.hash(newUser.password, salt, (error, hash) => {
-            if (error) {
-              throw error;
+    User
+      .findOne({ email })
+      .then((user) => {
+        if (user) {
+          res.status(BAD_REQUEST).json({ message: 'user with this email already exists.' });
+        } else {
+          // Hash password
+          genSalt(10, (err, salt) => {
+            if (err) {
+              throw err;
             }
-            newUser.password = hash;
-            newUser.save().then((createdUser, dbError) => {
-              if (dbError) {
-                res
-                  .status(BAD_REQUEST)
-                  .json({ message: dbError });
+
+            hash(password, salt, (error, hashed) => {
+              if (error) {
+                throw error;
               }
-              res
-                .json(
-                  {
-                    user: {
-                      email: createdUser.email,
-                      userType: createdUser.userType,
-                      id: createdUser.id,
-                    },
-                  },
-                );
+
+              const newUser = new User({
+                email,
+                password: hashed,
+                userType,
+              });
+
+              newUser
+                .save()
+                .then((createdUser, dbError) => {
+                  if (dbError) {
+                    throw dbError;
+                  }
+
+                  res.json({ user: pick(createdUser, ['email, id, usedType']) });
+                });
             });
           });
-        });
-      }
-    });
+        }
+      })
+      .catch((error) => {
+        res.status(INTERNAL_SERVER_ERROR).end();
+        logger.error(error);
+      });
   }
 });
 module.exports = router;
