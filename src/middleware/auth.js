3f96acb57d8c11fab526c663f9340efcd28099eb
inglec <inglec@tcd.ie>
2019-03-08 15:51:05 +0000

    Auth: Use "jwt.verify" asynchronously

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,26 +5,22 @@ const { JWT_SECRET: jwtSecret } = process.env;
 
 function auth(req, res, next) {
   const token = req.header('Authorization');
+
   if (!token) {
     res
       .status(UNAUTHORIZED)
-      .json({ message: 'No token supplied, access denied' });
+      .json({ message: 'no token supplied, access denied' });
   } else {
-    try {
-      const userId = jwt.verify(token, jwtSecret);
-      if (!userId) {
+    jwt.verify(token, jwtSecret, (error, decoded) => {
+      if (error) {
         res
           .status(UNAUTHORIZED)
-          .json({ message: 'Invalid token, access denied' });
+          .json({ message: 'failed to authenticate, permission denied' });
       } else {
-        req.user = userId;
+        req.user = decoded;
         next();
       }
-    } catch (e) {
-      res
-        .status(UNAUTHORIZED)
-        .json({ message: 'Failed to authenticate, permission denied.' });
-    }
+    });
   }
 }
 module.exports = auth;
